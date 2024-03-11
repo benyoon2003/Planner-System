6605b78e2024a69fbea61120b341a88f1b817ad7
benyoon2003 <benyoon2003@gmail.com>
2024-03-11 19:38:46 -0400

    Constructor for event

--- code/Event.java
+++ code/Event.java
@@ -9,4 +9,17 @@ public class Event {
   String endDay;
   int endTime;
   List<String> invitedUsers;
+
+  Event(String name, String location, boolean online,
+        String startDay, int startTime, String endDay,
+        int endTime, List<String> invitedUsers) {
+    this.name = name;
+    this.location = location;
+    this.online = online;
+    this.startDay = startDay;
+    this.startTime = startTime;
+    this.endDay = endDay;
+    this.endTime = endTime;
+    this.invitedUsers = invitedUsers;
+  }
 }
