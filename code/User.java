54b36eee46f918e734bce32fdedc4bc1e9306bbc
benyoon2003 <benyoon2003@gmail.com>
2024-03-11 20:01:04 -0400

    User constructor

--- code/User.java
+++ code/User.java
@@ -1,6 +1,11 @@
 import java.util.List;
 
 public class User {
+  String uid;
   List<Event> schedule;
 
+  public User(String uid, List<Event> schedule) {
+    this.uid = uid;
+    this.schedule = schedule;
+  }
 }
