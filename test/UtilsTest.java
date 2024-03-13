0a2a377b567c6bf4550f2c8c2dbb2d871e6ab879
benyoon2003 <benyoon2003@gmail.com>
2024-03-12 21:13:46 -0400

    Automatically make host inside invitees

--- test/UtilsTest.java
+++ test/UtilsTest.java
@@ -12,19 +12,31 @@ import model.Utils;
 public class UtilsTest {
 
   Event event1;
+  Event event2;
+  Event event3;
+  Event event4;
   User user1;
+  User user2;
+  User user3;
+  User user4;
 
   @Before
   public void setup() {
     event1 = new Event("event1","Snell", true,
             Day.Monday, 600, Day.Tuesday, 800, new ArrayList<>());
+
     user1 = new User("Ben", List.of(event1));
+
+    event2 = new Event("event2", "Churchill", false,
+            Day.Saturday, 300, Day.Monday, 400, new ArrayList<>(List.of(user1)));
+
+    user2 = new User("Nico", List.of(event2));
   }
 
   // TODO: Test with bigger schedule
   @Test
   public void writeToFile() {
-    Utils.writeToFile(user1);
+    Utils.writeToFile(user2);
   }
 
 
