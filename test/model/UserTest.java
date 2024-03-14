1fef25ed45f29d0f1e8d5f48176a5c9ee7f22808
Nico Cruz <BoogalooCruz@gmail.com>
2024-03-13 22:11:15 -0400

    Finished the User and Event test suite

--- test/model/UserTest.java
+++ test/model/UserTest.java
@@ -10,10 +10,24 @@ public class UserTest {
 
   User example;
 
+  Event one;
+
+  Event two;
+
+  private void ExampleSchedule(){
+    one = new Event("Host Event", "Snell", false,
+            Day.Monday, 1000, Day.Tuesday, 1800, List.of());
+    two = new Event("Host Event", "Snell", false,
+            Day.Sunday, 1200, Day.Monday, 800, List.of());
+    example = new User("Example", List.of(one, two));
+    one.setInvitedUsers(List.of(example));
+    two.setInvitedUsers(List.of(example));
+  }
+
   private void ExampleConflictSchedule(){
-    Event one = new Event("Host Event", "Snell", false,
+    one = new Event("Host Event", "Snell", false,
             Day.Monday, 1000, Day.Thursday, 1800, List.of());
-    Event two = new Event("Host Event", "Snell", false,
+    two = new Event("Host Event", "Snell", false,
             Day.Monday, 1200, Day.Tuesday, 1800, List.of());
     example = new User("Conflict", List.of(one, two));
     one.setInvitedUsers(List.of(example));
@@ -21,11 +35,31 @@ public class UserTest {
   }
 
   private void Example2ConflictSchedule(){
-    Event one = new Event("Host Event", "Snell", false,
+    one = new Event("Host Event", "Snell", false,
             Day.Friday, 1000, Day.Thursday, 1800, List.of());
-    Event two = new Event("Host Event", "Snell", false,
+    two = new Event("Host Event", "Snell", false,
             Day.Saturday, 1200, Day.Saturday, 1800, List.of());
     example = new User("Conflict", List.of(one, two));
+    one.setInvitedUsers(List.of(example));
+    two.setInvitedUsers(List.of(example));
+  }
+
+  private void ExampleAddEvent(){
+    one = new Event("Host Event", "Snell", false,
+            Day.Monday, 1000, Day.Tuesday, 1800, List.of());
+    two = new Event("Host Event", "Snell", false,
+            Day.Sunday, 1200, Day.Sunday, 1800, List.of());
+    example = new User("Example", List.of(one));
+    one.setInvitedUsers(List.of(example));
+    two.setInvitedUsers(List.of(example));
+  }
+
+  private void ExampleAddEventInvalid(){
+    one = new Event("Host Event", "Snell", false,
+            Day.Monday, 1000, Day.Thursday, 1800, List.of());
+    two = new Event("Host Event", "Snell", false,
+            Day.Monday, 1200, Day.Tuesday, 1800, List.of());
+    example = new User("Conflict", List.of(one));
     one.setInvitedUsers(List.of(example));
     two.setInvitedUsers(List.of(example));
   }
@@ -37,5 +71,30 @@ public class UserTest {
   @Test(expected = IllegalArgumentException.class)
   public void test2Conflict(){
     Example2ConflictSchedule();
+  }
+
+  @Test
+  public void testSortEvents(){
+    ExampleSchedule();
+    assertEquals(this.example.schedule.get(0), two);
+  }
+
+  @Test
+  public void testAddEvent(){
+    ExampleAddEvent();
+    this.example.addEvent(two);
+    assertEquals(this.example.schedule.get(0), two);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidAddEvent(){
+    ExampleAddEventInvalid();
+    this.example.addEvent(two);
+  }
+
+  @Test
+  public void testEventOnDay(){
+    ExampleSchedule();
+    assertEquals(this.example.eventsOnDay(Day.Monday), List.of(two, one));
   }
 }
