b149afdf8a60f32b59a7b3fa9f24a70e1d961b12
Nico Cruz <BoogalooCruz@gmail.com>
2024-03-27 17:28:13 -0400

    Added main class and comments

---
+++ code/PlannerRunner.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Day;
+import model.NuPlanner;
+import view.MainScheduleFrameView;
+
+public final class PlannerRunner {
+  /**
+   * This method is used to test the view and see it on a test model of users.
+   * We used this to see how our implementation looked and what bugs there were.
+   * @param args
+   */
+  public static void main(String[] args) {
+    NuPlanner testModel = new NuPlanner(new ArrayList<>());
+    testModel.addUser("Ben");
+    testModel.addUser("Nico");
+    testModel.addUser("Lucia");
+    testModel.createEvent("Ben", "Working on OOD", "Snell", false,
+            Day.Monday, 1000, Day.Wednesday, 2055, List.of());
+    testModel.createEvent("Nico", "Also working on OOD", "Snell", true,
+            Day.Thursday, 500, Day.Saturday, 2000, List.of());
+    testModel.createEvent("Lucia", "Also working on OOD", "Meserve", true,
+            Day.Sunday, 500, Day.Sunday, 900, List.of("Ben", "Nico"));
+    new MainScheduleFrameView(testModel);
+
+  }
+}
